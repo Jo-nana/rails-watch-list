6881ca92815edd6151169b1b86e70a2e02d3050a
Johanna Florange <florangejohanna@gmail.com>
2022-05-19 17:06:37 +0200

    problem with list input for the bookmarks form to fix

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,23 @@
 class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(params_bookmark)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_bookmark
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
 end
