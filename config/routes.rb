f42b6d208c9742dc9724433756e8f9dc5d490e50
Johanna Florange <florangejohanna@gmail.com>
2022-05-19 16:24:15 +0200

    index, show, new, create added for the list | bookmark controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'lists', to: 'lists#index'
+  # Display the lists
   root to: 'lists#index'
+  # Display what's inside a list
+  resources :lists, only: [ :show, :new, :create ] do
+    # create and delete new bookmarks
+    resources :bookmarks, only: [:new, :create, :destroy ]
+  end
 end
